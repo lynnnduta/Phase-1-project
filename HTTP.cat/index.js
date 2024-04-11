51f1c73e2f2d7974627a2cedd915ac3ee1d2c6f8
lynnnduta <162110757+lynnnduta@users.noreply.github.com>
2024-04-12 02:01:17 +0300

    Update index.js

--- HTTP.cat/index.js
+++ HTTP.cat/index.js
@@ -48,7 +48,7 @@ clearBtn.addEventListener('click', () => {
     catImageContainer.innerHTML = '';
 });
 
-// Event listener for Enter key press
+// Event listener for Enter 
 statusCodeInput.addEventListener('keypress', event => {
     if (event.key === 'Enter') {
         const statusCode = statusCodeInput.value;
